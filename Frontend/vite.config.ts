123eb9d11c2ce5320303e2b409490a97756655ad
JivanAcharya <jivan9181@gmail.com>
2025-05-10 21:32:28 +0545

    fix: bug fixes

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -1,23 +1,25 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
-export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
-  optimizeDeps: {
-    include: ['lucide-react'],
-  },
-  server: {
-    port:8000,
-    proxy: {
-      '/api': {
-        target: 'http://40.81.255.102:8080',
-        changeOrigin: true,
-        secure: false,
-      }
-    }
-  }
+export default defineConfig(({ mode }) => {
+  // Load env variables based on current mode (e.g., development, production)
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    plugins: [react(), tailwindcss()],
+    optimizeDeps: {
+      include: ['lucide-react'],
+    },
+    server: {
+      port: 8000,
+      proxy: {
+        '/api': {
+          target: env.VITE_API_PROXY_URL,
+          changeOrigin: true,
+          secure: false,
+        },
+      },
+    },
+  };
 });
